4e16a75dffc037903417e348834c4184c41977ab
rita <ritazenabor@yahoo.comm>
2023-01-14 02:56:35 +0000

    I am loving this devops

---
+++ app.py
@@ -0,0 +1,3 @@
+print ('this is my first python project. I am going to learn pyton after now and ensure I can write in python') 
+print ('God will defintely help me in this devops journey and He will crown my effort with huge success in Jesus name. Amen')
+print ('God is my help')
---
+++ myapp.java
@@ -0,0 +1,3 @@
+I am God's favoured child
+My God is so good. He is been there for me even
+before I was conceive in my mother's womb.
